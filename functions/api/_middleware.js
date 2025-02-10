e28d7c3f4689a1c4e2431482356eef2a48b8043d
terry <297234995@qq.com>
2025-02-10 15:25:52 +0800

    1.add functionss

---
+++ functions/api/_middleware.js
@@ -0,0 +1,9 @@
+export const onRequest = async (context) => {
+  const { request } = context;
+  const url = new URL(request.url);
+  url.hostname = url.hostname.replace(/-test\./, ".");
+  const newRequest = new Request(url, request);
+  console.log(`call :${newRequest.url.hostname}`)
+  const response = fetch(newRequest);
+  return response;
+};
