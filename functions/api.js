83022b14cc77273a911f3b7f9583d23a2a5491ee
terry <297234995@qq.com>
2025-02-10 12:40:30 +0800

    1.fix replace

--- functions/api.js
+++ functions/api.js
@@ -1,8 +1,9 @@
 export const onRequest = async (context) => {
   const { request } = context;
   const url = new URL(request.url);
-  url.hostname = url.hostname.replace(/-test\./, ".");
+  url.hostname = url.hostname.replace("-test", "");
   const newRequest = new Request(url, request);
+  console.log(newRequest)
   const response = fetch(newRequest);
   // const response = await context.next();
   response.headers.set("Access-Control-Allow-Origin", "*");
