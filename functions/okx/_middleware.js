829f6ace6c29194012af5c6f8a1e32d27b27d0f0
terry <297234995@qq.com>
2025-02-10 15:43:06 +0800

    1.change okx

--- functions/okx/_middleware.js
+++ functions/okx/_middleware.js
@@ -1,7 +1,7 @@
 export const onRequest = async (context) => {
     const { request } = context;
     const url = new URL(request.url);
-    url.hostname = url.hostname.replace(/-test\./, ".");
+    url.hostname = "www.okx.com";
     const newRequest = new Request(url, request);
     console.log(`call :${newRequest.url.hostname}`)
     const response = fetch(newRequest);
