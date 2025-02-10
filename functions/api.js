6fc4dd8e949d62c447a4c7d739918c398d391db7
terry <297234995@qq.com>
2025-02-10 12:09:20 +0800

    1.test

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,11 @@
-export const onRequest=async (context) => {
-    const response = await context.next();
-    response.headers.set('Access-Control-Allow-Origin', '*');
-    response.headers.set('Access-Control-Max-Age', '86400');
-    return response;
-  };
+export const onRequest = async (context) => {
+  const { request } = context;
+  const url = new URL(request.url);
+  url.hostname = url.hostname.replace(/-test\./, ".");
+  const newRequest = new Request(url, request);
+  const response = fetch(newRequest);
+  // const response = await context.next();
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Max-Age", "86400");
+  return response;
+};
