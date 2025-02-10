9f8a98a83726f648b46c11de28c28cf1fd48aac6
terry <297234995@qq.com>
2025-02-10 17:34:40 +0800

    1.rm log

--- functions/okx/_middleware.js
+++ functions/okx/_middleware.js
@@ -3,7 +3,7 @@ export const onRequest = async (context) => {
     const url = new URL(request.url);
     url.hostname = url.hostname.replace(/-test\./, ".");
     const newRequest = new Request(url, request);
-    console.log(`call :${newRequest.url.hostname}`)
+    // console.log(`call :${newRequest.url.hostname}`)
     const response = fetch(newRequest);
     return response;
   };
