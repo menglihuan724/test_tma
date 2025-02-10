693cf5dc2d888b9c0003489d5ef161ce422cf6d2
terry <297234995@qq.com>
2025-02-10 15:13:50 +0800

    1.add log

--- functions/api/_middleware.js
+++ functions/api/_middleware.js
@@ -3,7 +3,7 @@ export const onRequest = async (context) => {
   const url = new URL(request.url);
   url.hostname ="faku.cflpool.io"
   const newRequest = new Request(url, request);
-  console.log(`test :${newRequest}`)
+  console.log(`test :${newRequest.hostname}`)
   const response = fetch(newRequest);
   // const response = await context.next();
   // response.headers.set("Access-Control-Allow-Origin", "*");
